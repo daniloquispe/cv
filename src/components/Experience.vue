bc32b3bfb2163b6fa512a65b82763c55ef119964
Danilo Quispe <daniloquispe@gmail.com>
2022-07-06 00:52:22 -0500

    Update experience section

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,35 +1,36 @@
 <template>
 	<RightSideSection title="Experience">
-		<ExperienceItem period="2021 - today" company-name="Bilingual S.A.C." title="Developer">
+		<ExperienceItem period="2021 - today" company-name="Bilingual Language Solutions" title="Developer">
 			<p>Development of integration software for Bilingual interpreters' information and productivity
 				reports (web-based platform, REST API and Google Chrome extension).</p>
 			<p>Integration with AWS and Pusher.</p>
+			<p>Calling data extraction from Asterisk database.</p>
 			<p>Application testing and deployment.</p>
 			<p>Employees training.</p>
 			<p>User manuals and technical documentation</p>
 		</ExperienceItem>
 		<ExperienceItem period="2021" company-name="New Information Digital" title="Web Developer">
-			<p>Modifications for Perumin website (custom CMS).</p>
+			<p>Modifications and mew modules development for Perumin website (3rd-party custom CMS).</p>
 		</ExperienceItem>
-		<ExperienceItem period="2020 - 2021" company-name="Informática Delta S.A.C." title="PHP Developer">
+		<ExperienceItem period="2020 - 2021" company-name="Informática Delta" title="PHP Developer">
 			<p>Software development for finance/retail clients.</p>
 			<p>Integration with AWS.</p>
-			<p>Migration from desktop (legacy) software versions (Visual Basic .NET and SQL Server)</p>
+			<p>Migration from desktop (legacy) software (Visual Basic .NET and SQL Server)</p>
 			<p>Junior programmers technical interview and selection.</p>
 		</ExperienceItem>
-		<ExperienceItem period="2017 - 2018" company-name="Corporación Efameinsa e Ingeniería S.A." title="Web programmer">
+		<ExperienceItem period="2017 - 2018" company-name="Corporación Efameinsa e Ingeniería" title="Web programmer">
 			<p>WordPress theme customization, enhancements and bugfixes (3rd-party development)</p>
-			<p>Migration from WordPress to OpenCart.</p>
-			<p>OpenCart theme customizations.</p>
+			<p>CMS migration, from WordPress to OpenCart.</p>
+			<p>OpenCart 3rd-party themes customization.</p>
 			<p>Webmaster.</p>
 			<p>Tech support assistance.</p>
-			<p>coordination with hosting and e-mail provider.</p>
+			<p>Coordination with hosting and e-mail providers.</p>
 		</ExperienceItem>
 		<ExperienceItem period="2012 - today" company-name="ISOL Perú: Soluciones en Internet" title="Web & Software Developer">
-			<p>My own development business and commercial name</p>
+			<p>My own development business and commercial name.</p>
 			<p>Web and software development, hosting and domains.</p>
-			<p>Clients training, help desk and remote technical support</p>
-			<p>Coordination with external suppliers (Spanish and English)</p>
+			<p>Clients training, help desk and remote technical support.</p>
+			<p>Coordination with external providers (Spanish and English).</p>
 		</ExperienceItem>
 	</RightSideSection>
 </template>
